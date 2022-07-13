69c4968edfdf06bc1a662994ca4933004329e0fa
Pranay Boreddy <pranayboreddy@gmail.com>
2022-07-13 18:15:38 +0530

    Added link highlights and scroll up icon on scroll

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,18 +1,59 @@
-import { UilEstate } from "@iconscout/react-unicons";
-import { UilUser } from "@iconscout/react-unicons";
-import { UilFileAlt } from "@iconscout/react-unicons";
-import { UilBriefcaseAlt } from "@iconscout/react-unicons";
-import { UilScenery } from "@iconscout/react-unicons";
-import { UilMessage } from "@iconscout/react-unicons";
-import { UilTimes } from "@iconscout/react-unicons";
-import { UilApps } from "@iconscout/react-unicons";
-import { useState } from "react";
+import {
+  UilEstate,
+  UilUser,
+  UilFileAlt,
+  UilBriefcaseAlt,
+  UilScenery,
+  UilMessage,
+  UilTimes,
+  UilApps,
+  UilArrowUp,
+} from "@iconscout/react-unicons";
+import { useState, useEffect, useRef } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [hightLightId, setHighlightId] = useState("home");
+  const [isScrolled, setScrolled] = useState(false);
+  const sectionref = useRef(null);
+
+  const scrollSectionHOC = () => {
+    setActiveLink(sectionref.current);
+  };
+
+  const setActiveLink = (refs) => {
+    const scrollY = window.pageYOffset;
+    refs.forEach((current) => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 20;
+      const sectionId = current.getAttribute("id");
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        setHighlightId(sectionId);
+      }
+    });
+
+    // Adding box shadow to  Nav for better visibility if page if scrolled to bottom
+    if (scrollY >= 80) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    sectionref.current = document.querySelectorAll("section[id]");
+    window.addEventListener("scroll", scrollSectionHOC);
+    return () => {
+      window.removeEventListener("scroll", scrollSectionHOC);
+    };
+  }, []);
+
   return (
-    <header className="header" id="header">
+    <header
+      className={`header ${isScrolled ? "scrollHeader" : ""}`}
+      id="header"
+    >
       <nav className="nav container">
         <a href="./" className="logo">
           Pranay
@@ -20,37 +61,67 @@ const Navbar = () => {
         {showMenu && (
           <div className="menu">
             <ul className="list grid">
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "home" ? "activeLink" : ""
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#home" className="link">
                   <UilEstate size="26" />
                   Home
                 </a>
               </li>
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "about" ? "activeLink" : ""
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#about" className="link">
                   <UilUser size="26" />
                   About
                 </a>
               </li>
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "skills" ? "activeLink" : ""
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#skills" className="link">
                   <UilFileAlt size="26" />
                   Skills
                 </a>
               </li>
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "services" ? "activeLink" : ""
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#services" className="link">
                   <UilBriefcaseAlt size="26" />
                   Services
                 </a>
               </li>
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "portfolio" ? "activeLink" : ""
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#portfolio" className="link">
                   <UilScenery size="26" />
                   Portfolio
                 </a>
               </li>
-              <li className="item" onClick={() => setShowMenu(false)}>
+              <li
+                className={`item ${
+                  hightLightId === "contact_me" && "activeLink"
+                }`}
+                onClick={() => setShowMenu(false)}
+              >
                 <a href="#contact_me" className="link">
                   <UilMessage size="26" />
                   Contact me
@@ -66,6 +137,13 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
+      <a
+        href="#"
+        className={`scrollup ${isScrolled ? "show" : ""}`}
+        id="scroll-up"
+      >
+        <UilArrowUp size="1.6rem" className="icon" />
+      </a>
     </header>
   );
 };
